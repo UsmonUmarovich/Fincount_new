2f8194a245dbdff06e4155dfe1ea61485bd471e1
Usmon Umarovich Reyimberganov <usmon@192.168.1.4>
2023-04-15 23:48:28 +0500

    BETA2 | completed Home page full, and now About

--- index.js
+++ index.js
@@ -10,3 +10,18 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach((el) => observer.observe(el));
+
+menuIcon = document.querySelector(".menu-icon");
+menuIcon.onclick = function () {
+  menuLinks = document.querySelector(".menu-links");
+  menuLinks.classList.toggle("active");
+  blurscreen = document.querySelector(".blur");
+  blurscreen.classList.toggle("active");
+  menu = document.querySelector(".menu");
+  menu.classList.toggle("active");
+};
+
+TranslateMenuIcon = document.querySelector(".translate-menu-icon");
+TranslateMenuIcon.onclick = function () {
+  TranslateMenuIcon.classList.toggle("active");
+};
